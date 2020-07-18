2a949f8045e480732f022272e41ebee75999f89f
bennivalur <bennivalur@gmail.com>
2020-07-18 17:05:23 +0000

    moved stuff into directories

--- getData.py
+++ getData.py
@@ -9,6 +9,8 @@ def getImage(path):
 
 def getD(year_from, year_to, columns, file_descr):
 
+    print("Getting data and fields needed")
+
     YEARS = range(year_from, year_to+1)
     year_to = str(year_to)
     year_from = str(year_from)
@@ -19,29 +21,30 @@ def getD(year_from, year_to, columns, file_descr):
         i_data = pd.read_csv('https://github.com/guga31bb/nflfastR-data/blob/master/data/' \
                             'play_by_play_' + str(i) + '.csv.gz?raw=True',
                             compression='gzip', low_memory=False)
-        print("drop", i)
+        print("Drop unneeded columns", i)
         i_data.drop(['passer_player_name', 'passer_player_id',
             'rusher_player_name', 'rusher_player_id',
             'receiver_player_name', 'receiver_player_id'],
             axis=1, inplace=True)
-        print("reg", i)
+
+        print("Select only regular season", i)
         i_data = i_data.loc[i_data.season_type=='REG']
-        print("run pass no",i)
+
+        print("Select only run, pass and no plays",i)
         i_data = i_data.loc[(i_data.play_type.isin(['no_play','pass','run'])) & (i_data.epa.isna()==False)]
         i_data.play_type.loc[i_data['pass']==1] = 'pass'
         i_data.play_type.loc[i_data.rush==1] = 'run'
-        print("index reset",i)
-        i_data.reset_index(drop=True, inplace=True)
-        print('remove columns')
+
+        print('Remove columns we don\'t need')
         i_data = i_data[columns]
 
         data = data.append(i_data, sort=True)
 
-    print("reset index")
+    print("Reset Index")
     data.reset_index(drop=True, inplace=True)
 
-    print("save to csv")
-    data.to_csv(file_descr + '_play_by_play_'+ year_from +'_to_'+ year_to +'.csv.gz', compression='gzip')
+    print("Save to csv")
+    data.to_csv(file_descr + '/play_by_play_'+ year_from +'_to_'+ year_to +'.csv.gz', compression='gzip')
 
 
 
--- hotcold.py
+++ hotcold.py
@@ -4,15 +4,15 @@ import os
 import urllib.request
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 import numpy as np
+from getData import getD
 
 def getImage(path): 
     return OffsetImage(plt.imread(path), zoom=.5)
 
 
-def makegraphs(year_from, year_to,descr):
+def makegraphs(year_from, year_to,descr, cold,hot):
 
-
-    data = pd.read_csv(descr + '_play_by_play_1999_to_2019.csv.gz', compression='gzip', low_memory=False)
+    data = pd.read_csv(descr + '/play_by_play_1999_to_2019.csv.gz', compression='gzip', low_memory=False)
 
     logos = os.listdir(os.getcwd() + '/logos')
 
@@ -21,27 +21,24 @@ def makegraphs(year_from, year_to,descr):
     for i in logos:
         logo_paths.append(os.getcwd() + '/logos/' + str(i))
 
-    cold_limit = 35
-    heat_limit = 80
+    cold_limit = cold
+    heat_limit = hot
 
     #Filter to pass plays,temp and season range and groupby offensive team
     team_epa = data.loc[(data['temp']  <= cold_limit) & (data['play_type']== 'pass') & (data['season'] >= year_from ) & (data['season'] <= year_to )].groupby(['posteam'])[['epa']].mean()
 
-    #Do the same but for rushing plays
+    #Do the same but for games in heat
     team_epa['hot'] = data.loc[(data['temp']  >= heat_limit) & (data['play_type'] == 'pass') & (data['season'] >= year_from ) & (data['season'] <= year_to )].groupby('posteam')[['epa']].mean()
 
+    #Text files containing the data
     coldtx = data.loc[(data['temp']  <= cold_limit) & (data['play_type']== 'pass') & (data['season'] >= year_from ) & (data['season'] <= year_to )].groupby(['posteam','game_id']).agg({'epa':'mean','temp':'mean'})
     hottx = data.loc[(data['temp']  >= heat_limit) & (data['play_type'] == 'pass') & (data['season'] >= year_from ) & (data['season'] <= year_to )].groupby(['posteam','game_id']).agg({'epa':'mean','temp':'mean'})
     
-    with open(descr + '/data.txt', 'w') as file:
-        file.write(team_epa.to_string())
-
     with open(descr + '/hot.txt', 'w') as file:
         file.write(hottx.to_string())
 
     with open(descr + '/cold.txt', 'w') as file:
         file.write(coldtx.to_string())
-
 
     #Define x and y
     x = team_epa['epa']
@@ -50,9 +47,7 @@ def makegraphs(year_from, year_to,descr):
     #Create a figure with size 12x12
     fig, ax = plt.subplots(figsize=(15,15))
 
-
-    
-
+    #Fixed size of graph, good for making animations
     #ax.set_xlim([-0.45,0.45])
     #ax.set_ylim([-0.45,0.47])
 
@@ -81,17 +76,24 @@ def makegraphs(year_from, year_to,descr):
     plt.figtext(.81, .07, 'Data: nflfastR', fontsize=12)
     plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)), 
             color='darkorange', linestyle='--')
+
     #Save the figure as a png
     plt.savefig(descr + '/passing_heat_epas_' + year_from + '_' + year_to + '.png', dpi=400)
 
+
+#Set up the datafile if it does not exist
+columns = ['game_id','posteam','play_type','epa','temp','season']
+descr  = "temp"
+
+#creates datafile
+getD(1999, 2019, columns,descr)
 
 year_from = 1999
 year_to = 2019
 
 years = range(1999,2020)
-descr = 'temp'
 #for i in years:
 #    print(i)
 #    if(i <= (year_to -9)):
 #        makegraphs(i,i+9,descr)
-makegraphs(2012,2015,descr)
+makegraphs(1999,2010,descr,35,80)
